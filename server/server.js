24b291a7538fca3e21380d9936df1fe7eb67c6dc
Enes Tasbasi <enestasbasi@outlook.com>
2018-05-31 01:20:24 -0400

    add the PATCH

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -35,14 +36,62 @@ app.get('/todos/:id', (req, res) => {
         Todo.findById({
             _id: id
         }).then((todo) => {
+            if(!todo) {
+                return res.status(404).send();
+            }
             return res.send({todo});
         }).catch((e) => {
-            return res.status(400).send({});
+            return res.status(400).send();
         });
     } else {
-        return res.status(400).send({});
+        return res.status(400).send();
     }
 });
+
+app.delete('/todos/:id', (req, res) => {
+   let id = req.params.id;
+
+   if(!ObjectID.isValid(id)) {
+       return res.status(400).send();
+   }
+
+   Todo.findByIdAndRemove(id).then((result) => {
+       if(!result) {
+           return res.status(404).send();
+       }
+       return res.status(200).send(result);
+   }).catch((e) => {
+       res.status(404).send();
+   })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
+
 
 app.listen(port, () => console.log(`Server up on port ${port}.`));
 
